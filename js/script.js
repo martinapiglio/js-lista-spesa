1e3b5cc1f823da758039ebb5878a75ce101f19f4
martinapiglio <martina.piglio@gmail.com>
2023-03-01 15:11:05 +0100

    js pseudo-code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 console.log('test');
+
+/*
+1 - create variables:
+    a - array shopping list;
+    b - ul list from HTML;
+    c - an index set as 0;
+2 - WHILE cycle:
+    a - until index remains < shopping list array variable:
+        i - create a new element (new list item);
+        ii - append this new element to the shopping list array;
+        iii - write the list item value;
+        iv - the index must be +1 in order to avoid the infinite cycle;
+3 - insert a button in order to add new elements to the list.
+*/
